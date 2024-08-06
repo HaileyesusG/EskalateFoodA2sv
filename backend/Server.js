233996eaf6cd98deb9c87f90659c3873594d76d3
HaileyesusG <hailegetnet2020@gmail.com>
2024-08-06 15:12:08 +0300

    thirtyfour

--- backend/Server.js
+++ backend/Server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://africadeployfrontend.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
