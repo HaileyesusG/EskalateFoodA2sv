cba0ac1b7defdef3ef05f747e15fdda0a475c92d
HaileyesusG <hailegetnet2020@gmail.com>
2024-11-15 15:55:38 +0300

    yets77

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -613,6 +613,27 @@ const Home = ({ user3 }) => {
     setdisplay8("visible");
     calculateDistance();
   };
+  // Modal component
+  function TaskAcceptedModal() {
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+        <div className="bg-white rounded-lg shadow-lg p-4 sm:p-6 max-w-full sm:max-w-md mx-4 sm:mx-auto">
+          <h2 className="text-lg sm:text-2xl font-bold mb-4 text-center">
+            Task Already Accepted
+          </h2>
+          <p className="text-gray-700 mb-4 text-center text-sm sm:text-base">
+            Another technician has already accepted this task.
+          </p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            onClick={() => window.location.reload()}
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={""}>
       <div className="App p-4">
@@ -1065,26 +1086,5 @@ const Home = ({ user3 }) => {
     </div>
   );
 };
-// Modal component
-function TaskAcceptedModal() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-4 sm:p-6 max-w-full sm:max-w-md mx-4 sm:mx-auto">
-        <h2 className="text-lg sm:text-2xl font-bold mb-4 text-center">
-          Task Already Accepted
-        </h2>
-        <p className="text-gray-700 mb-4 text-center text-sm sm:text-base">
-          Another technician has already accepted this task.
-        </p>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
-          onClick={() => window.location.reload()}
-        >
-          OK
-        </button>
-      </div>
-    </div>
-  );
-}
 
 export default Home;
