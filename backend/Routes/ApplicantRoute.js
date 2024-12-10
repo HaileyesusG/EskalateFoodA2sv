2d322d9fcb6988627da6e53d0f5eb1b3f434523f
HaileyesusG <hailegetnet2020@gmail.com>
2024-12-10 14:15:12 +0300

    lo18

--- backend/Routes/ApplicantRoute.js
+++ backend/Routes/ApplicantRoute.js
@@ -10,55 +10,55 @@ const multer = require("multer");
 const { storage } = require("../MiddleWare/cloudinary");
 const { getOtp, deleteOtp } = require("../utils/otpStore");
 // Middleware for parsing form data without immediately uploading files
-const parseFormData = multer().fields([{ name: "testImages", maxCount: 4 }]);
 const upload = multer({ storage });
-
 const upload2 = upload.fields([{ name: "testImages", maxCount: 4 }]);
+
+const validateOtp = async (req, res, next) => {
+  try {
+    const { otp, email } = req.body;
+
+    // Validate OTP
+    const storedOtpData = getOtp(email);
+    if (!storedOtpData) {
+      return res.status(400).send({ message: "OTP not generated or expired" });
+    }
+
+    const isOtpValid = await bcryptjs.compare(otp, storedOtpData.otp);
+    const isOtpExpired = Date.now() > storedOtpData.otpExpiry;
+
+    if (isOtpExpired) {
+      delete otpStore[email];
+      return res.status(400).send({ message: "OTP expired" });
+    }
+
+    if (!isOtpValid) {
+      return res.status(400).send({ message: "Invalid OTP" });
+    }
+    // OTP is valid, remove from store
+    deleteOtp(email);
+    // Forward request to the controller
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 const express = require("express");
 const router = express.Router();
 router.route("/GetApplicant/:id").get(GetApplicant);
 router.route("/GetAllApplicant").get(GetAllApplicant);
 router.route("/ApplicantCreate").post(
-  parseFormData,
-  async (req, res, next) => {
-    try {
-      const { otp, email } = req.body;
-
-      // Validate OTP
-      const storedOtpData = getOtp(email);
-      if (!storedOtpData) {
-        return res
-          .status(400)
-          .send({ message: "OTP not generated or expired" });
+  validateOtp,
+  (req, res, next) => {
+    // OTP is valid, upload files
+    upload2(req, res, (err) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
       }
 
-      const isOtpValid = await bcryptjs.compare(otp, storedOtpData.otp);
-      const isOtpExpired = Date.now() > storedOtpData.otpExpiry;
-
-      if (isOtpExpired) {
-        delete otpStore[email];
-        return res.status(400).send({ message: "OTP expired" });
-      }
-
-      if (!isOtpValid) {
-        return res.status(400).send({ message: "Invalid OTP" });
-      }
-      // OTP is valid, remove from store
-      deleteOtp(email);
-
-      // OTP is valid, upload files
-      upload2(req, res, (err) => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-
-        // Forward request to the controller
-        next();
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
+      next();
+    });
   },
+
   ApplicantCreate
 );
 router.route("/GenerateOtp").post(GenerateOtp);
