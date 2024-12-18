4b19d7b4099694f20bbf1da3916cde5bdb59ac16
haile <hailegetnet2020@gmail.com>
2024-12-18 21:55:13 +0300

    t3

--- frontend/src/Components/FrontPage.jsx
+++ frontend/src/Components/FrontPage.jsx
@@ -3,10 +3,11 @@ import { IoCall } from "react-icons/io5";
 import { IoMail } from "react-icons/io5";
 import React, { useState, useEffect } from "react";
 import hiloe from "../assets/AiM.jpeg";
+import x from '../assets/'
 import hiloe2 from "../assets/AiM2.jpeg";
-import sport3 from "../assets/sport3.png";
-import sport2 from "../assets/sport2.png";
-import sport4 from "../assets/sport4.png";
+import sport3 from "../assets/SPORT3.png";
+import sport2 from "../assets/SPORT2.png";
+import sport4 from "../assets/SPORT4.png";
 import hiloe4 from "../assets/AiM4.jpeg";
 import hiloe5 from "../assets/lg1.webp";
 import Varzish from "../assets/Varzish.jpg";
