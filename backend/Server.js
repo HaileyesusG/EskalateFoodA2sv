cb69e3b321dc3f11f9a9be246686a3c2e5469048
HaileyesusG <hailegetnet2020@gmail.com>
2025-02-16 14:10:41 +0300

    last1.2

--- backend/Server.js
+++ backend/Server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://MasterFix.onrender.com",
+    origin: "https://masterfix.onrender.com",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: "Content-Type,Authorization",
@@ -41,7 +41,7 @@ let boddy = [];
 const serv = http.createServer(app);
 const io = new Server(serv, {
   cors: {
-    origin: "https://MasterFix.onrender.com",
+    origin: "https://masterfix.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
